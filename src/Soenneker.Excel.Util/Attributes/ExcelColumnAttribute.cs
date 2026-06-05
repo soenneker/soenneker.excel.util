7392851ac1342ff9272b60b9abe80d49c9833f46
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:33 -0500

    xml commenting

--- src/Soenneker.Excel.Util/Attributes/ExcelColumnAttribute.cs
+++ src/Soenneker.Excel.Util/Attributes/ExcelColumnAttribute.cs
@@ -2,8 +2,14 @@
 
 namespace Soenneker.Excel.Util.Attributes;
 
+/// <summary>
+/// Represents the excel column attribute.
+/// </summary>
 [AttributeUsage(AttributeTargets.Property)]
 public sealed class ExcelColumnAttribute(string name) : Attribute
 {
+    /// <summary>
+    /// Gets name.
+    /// </summary>
     public string Name { get; } = name;
 }
